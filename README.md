f7cc6ee33e10a17a91597c13219012a06cbbdfe8
breadleaf <bkhnapa@gmail.com>
2024-04-27 23:40:42 -0600

    restructured project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go Script
+
+Go script is a scripting language with a standalone cli tool or can be used as a library and embedded into Go programs.
--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,4 @@
-package goscript
+package main
 
 import (
 	"github.com/breadleaf/goscript/internal/lexer"
@@ -7,4 +7,14 @@ import (
 func Execute(program []string) {
 	l := lexer.NewLexer(program)
 	l.Lex()
+}
+
+func main() {
+	program := []string{
+		"var a = 1",
+		"var b = 2",
+		"var c = a + b",
+	}
+
+	Execute(program)
 }
